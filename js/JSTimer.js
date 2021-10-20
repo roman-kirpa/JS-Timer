432210da0b87aeba7da4fc383f92ac279efbddd1
roman.kirpa <ghostdrs@gmail.com>
2021-10-20 23:46:58 +0300

    replace while to setTimeout

--- js/JSTimer.js
+++ js/JSTimer.js
@@ -11,23 +11,26 @@ var runTimer = function () {
     }
 
     var date = new Date(inputDate);
-  
-    while (true) {
+    var delay = 1000;
+    function timeoutLoop() {
         var currentDate = new Date();
         var differenceMilisecond = Math.abs(date - currentDate);
-        var days= Math.floor(differenceMilisecond / (1000 * 60 * 60 * 24));
+        var days = Math.floor(differenceMilisecond / (1000 * 60 * 60 * 24));
         var hours = Math.floor((differenceMilisecond % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((differenceMilisecond % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((differenceMilisecond % (1000 * 60)) / 1000);
-        ChangeElement('Days', days);
-        ChangeElement('Hours', hours);
-        ChangeElement('Minutes', minutes);
-        ChangeElement('Seconds', seconds);
+        SetInnerHtmlToElement('Days', days);
+        SetInnerHtmlToElement('Hours', hours);
+        SetInnerHtmlToElement('Minutes', minutes);
+        SetInnerHtmlToElement('Seconds', seconds);
+        setTimeout(timeoutLoop, delay);
     }
-    
+
+    setTimeout(timeoutLoop, delay);
+
 }
 
-function ChangeElement(elementId, value) {
+function SetInnerHtmlToElement(elementId, value) {
     var hoursElement = document.getElementById(elementId);
     hoursElement.innerHTML = value;
 }
